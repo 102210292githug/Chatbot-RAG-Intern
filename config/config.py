1de9e91fb45f540ce7ac505e282c2b376c2ab79c
Le Huynh Duc <95340358+102210292githug@users.noreply.github.com>
2025-02-24 13:43:13 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,10 +1,10 @@
 from langchain_openai import OpenAIEmbeddings
 
 ## API
-OPENAI_API_KEY = "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"
-QDRANT_SERVER = "http://35.94.42.185:6333"
-QDRANT_API_KEY = "MEKONGAI-QDRANT-PRIVATE-API-KEY"
-collection_name="duck_chatbotRAG_pdf"
+OPENAI_API_KEY = "T"
+QDRANT_SERVER = ""
+QDRANT_API_KEY = ""
+collection_name=""
 
 # Path
 pdf_path = "../data/upload/triethoc.pdf"
